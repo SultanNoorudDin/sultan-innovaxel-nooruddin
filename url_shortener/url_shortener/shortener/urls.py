304ba176137f5ff4f72eea2c32e5a9769f4449e7
Sultan Nooruddin <sultannooruddin4@gmail.com>
2025-04-20 12:33:16 +0500

    create api tested successfully

--- url_shortener/url_shortener/shortener/urls.py
+++ url_shortener/url_shortener/shortener/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('shorten/', views.CreateShortURL.as_view(), name='create'),
-    path('shorten/<str:shortCode>/', views.RetrieveShortURL.as_view(), name='retrieve'),
-    path('shorten/<str:shortCode>/update/', views.UpdateShortURL.as_view(), name='update'),
-    path('shorten/<str:shortCode>/delete/', views.DeleteShortURL.as_view(), name='delete'),
-    path('shorten/<str:shortCode>/stats/', views.GetStatsView.as_view(), name='stats'),
+
 ]
--- url_shortener/url_shortener/shortener/views.py
+++ url_shortener/url_shortener/shortener/views.py
@@ -7,29 +7,3 @@ from .serializers import ShortURLSerializer
 class CreateShortURL(generics.CreateAPIView):
     queryset = ShortURL.objects.all()
     serializer_class = ShortURLSerializer
-
-class RetrieveShortURL(generics.RetrieveAPIView):
-    lookup_field = 'shortCode'
-    queryset = ShortURL.objects.all()
-    serializer_class = ShortURLSerializer
-
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.accessCount += 1
-        instance.save()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-
-class UpdateShortURL(generics.UpdateAPIView):
-    lookup_field = 'shortCode'
-    queryset = ShortURL.objects.all()
-    serializer_class = ShortURLSerializer
-
-class DeleteShortURL(generics.DestroyAPIView):
-    lookup_field = 'shortCode'
-    queryset = ShortURL.objects.all()
-
-class GetStatsView(generics.RetrieveAPIView):
-    lookup_field = 'shortCode'
-    queryset = ShortURL.objects.all()
-    serializer_class = ShortURLSerializer
