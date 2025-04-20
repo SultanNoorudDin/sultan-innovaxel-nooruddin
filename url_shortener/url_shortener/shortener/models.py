e59712730712ed63b09b66d02b94a8fa445a02ce
Sultan Nooruddin <sultannooruddin4@gmail.com>
2025-04-20 19:10:49 +0500

    comments added

--- url_shortener/url_shortener/shortener/models.py
+++ url_shortener/url_shortener/shortener/models.py
@@ -1,4 +1,5 @@
-import string, random
+import string
+import random
 from django.db import models
 from django.utils.timezone import now
 
--- url_shortener/url_shortener/shortener/views.py
+++ url_shortener/url_shortener/shortener/views.py
@@ -3,10 +3,12 @@ from rest_framework.response import Response
 from .models import ShortURL
 from .serializers import ShortURLSerializer
 
+# create api
 class CreateShortURL(generics.CreateAPIView):
     queryset = ShortURL.objects.all()
     serializer_class = ShortURLSerializer
 
+# get api
 class RetrieveShortURL(generics.RetrieveAPIView):
     lookup_field = 'shortCode'
     queryset = ShortURL.objects.all()
@@ -19,15 +21,18 @@ class RetrieveShortURL(generics.RetrieveAPIView):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
     
+# update api
 class UpdateShortURL(generics.UpdateAPIView):
     lookup_field = 'shortCode'
     queryset = ShortURL.objects.all()
     serializer_class = ShortURLSerializer
 
+# delete api
 class DeleteShortURL(generics.DestroyAPIView):
     lookup_field = 'shortCode'
     queryset = ShortURL.objects.all()
 
+# see access frequency api
 class GetStatsView(generics.RetrieveAPIView):
     lookup_field = 'shortCode'
     queryset = ShortURL.objects.all()
